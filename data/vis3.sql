4ad82a63190b7c006ac82cfcbc72a6f0dc29d9dd
Katarina <kaisaf22@gmail.com>
2025-12-18 21:23:21 +0100

    Almost done with vis3

---
+++ data/vis3.sql
@@ -0,0 +1,54 @@
+SELECT * FROM Sales.SalesOrderHeader
+
+SELECT
+    MIN(OrderDate) AS MinDate,
+    MAX(OrderDate) AS MaxDate
+FROM Sales.SalesOrderHeader;
+
+SELECT 
+    YEAR(OrderDate) AS OrderYear,
+    MONTH(OrderDate) AS OrderMonth,
+    SUM(TotalDue) AS TotalSales
+FROM Sales.SalesOrderHeader
+WHERE 
+    OrderDate >= DATEADD(MONTH, -12, GETDATE())
+GROUP BY
+    YEAR(OrderDate),
+    MONTH(OrderDate)
+ORDER BY
+    OrderYear ASC,
+    OrderMonth ASC;
+
+
+-- Försöker ändra format på datum, det blir lättare att läsa:
+
+SELECT 
+    FORMAT(OrderDate, 'yyyy-MM') AS YearMonth,
+    SUM(TotalDue) AS TotalSales
+FROM Sales.SalesOrderHeader
+WHERE 
+    OrderDate >= (
+        SELECT DATEADD(MONTH, -12, MAX(OrderDate))
+        FROM Sales.SalesOrderHeader
+    )
+GROUP BY
+    FORMAT(OrderDate, 'yyyy-MM')
+ORDER BY
+    YearMonth ASC;
+
+
+-- Hade kunnat snygga upp med att få TotalSales litte mer läsbar
+
+SELECT 
+    FORMAT(OrderDate, 'yyyy-MM') AS YearMonth,
+    CAST(SUM(TotalDue) AS decimal(18,2)) AS TotalSales
+FROM Sales.SalesOrderHeader
+WHERE 
+    OrderDate >= (
+        SELECT DATEADD(MONTH, -12, MAX(OrderDate))
+        FROM Sales.SalesOrderHeader
+    )
+GROUP BY
+    FORMAT(OrderDate, 'yyyy-MM')
+ORDER BY
+    YearMonth ASC;
