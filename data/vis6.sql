c87ce44e081ea984a27102c6efbd9f08268d6c26
Katarina <kaisaf22@gmail.com>
2025-12-23 10:32:23 +0100

    Done with vis6

--- data/vis6.sql
+++ data/vis6.sql
@@ -1,3 +1,13 @@
 SELECT * FROM Sales.SalesTerritory
 SELECT * FROM Sales.SalesOrderHeader
 SELECT * FROM Sales.Customer
+
+SELECT 
+    st.Name AS RegionName,
+    COUNT(DISTINCT c.CustomerID) AS UniqueCustomers,
+    CAST(SUM(soh.TotalDue) AS decimal(18,2)) AS TotalSales
+FROM Sales.SalesOrderHeader soh
+JOIN Sales.SalesTerritory st ON soh.TerritoryID = st.TerritoryID
+JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
+GROUP BY st.Name
+ORDER BY TotalSales DESC;
