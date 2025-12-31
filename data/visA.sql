81c6fc57fb8493c99a669d2f51d1ab37eb1f6097
Katarina <kaisaf22@gmail.com>
2025-12-31 14:23:19 +0100

    Almost done with my project

--- data/visA.sql
+++ data/visA.sql
@@ -32,3 +32,32 @@ JOIN Production.ProductSubcategory psc ON p.ProductSubcategoryID = psc.ProductSu
 JOIN Production.ProductCategory pc ON psc.ProductCategoryID = pc.ProductCategoryID
 GROUP BY st.Name, pc.Name
 ORDER BY TotalSales DESC;
+
+
+--Finns säsongsmönster per region?
+
+SELECT
+    st.Name AS RegionName,
+    FORMAT(OrderDate, 'yyyy-MM') AS YearMonth,
+    CAST(SUM(TotalDue) AS decimal(18,2)) AS TotalSales
+FROM Sales.SalesOrderHeader soh  
+JOIN Sales.SalesTerritory st ON soh.TerritoryID = st.TerritoryID
+GROUP BY
+    FORMAT(OrderDate, 'yyyy-MM'), st.Name
+ORDER BY
+    YearMonth ASC, RegionName ASC;
+
+
+-- SQL jag kommer att använda för en grupperat stapeldiagram (Försäljning per region uppdelat på produktkategori):
+
+SELECT 
+    st.Name AS Region,
+    pc.Name AS Category,
+    SUM(sod.LineTotal) AS TotalSales
+FROM Sales.SalesOrderHeader soh
+JOIN Sales.SalesTerritory st ON soh.TerritoryID = st.TerritoryID
+JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
+JOIN Production.Product p ON sod.ProductID = p.ProductID
+JOIN Production.ProductSubcategory psc ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+JOIN Production.ProductCategory pc ON psc.ProductCategoryID = pc.ProductCategoryID
+GROUP BY st.Name, pc.Name;
